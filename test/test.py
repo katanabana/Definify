90f35ebf38ae4005504801d7a7bcd11175951646
katanabana <katanabana01@gmail.com>
2023-06-21 11:54:00 +0500

    remake V

--- test/test.py
+++ test/test.py
@@ -1,2 +1,43 @@
-a = 0
-print((a += 1))
+from random import randint
+from flask_socketio import SocketIO
+
+from flask import Flask, render_template, session
+from flask_login import LoginManager, UserMixin, AnonymousUserMixin, current_user
+
+
+class User:
+    pass
+
+
+class AuthenticatedUser(User, UserMixin):
+    pass
+
+
+class AnonymousUser(User, AnonymousUserMixin):
+    pass
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'sadfsadf'
+login_manger = LoginManager(app)
+login_manger.anonymous_user = AnonymousUser
+socketio = SocketIO(app)
+
+
+@login_manger.user_loader
+def load_user(user_id):
+    return AuthenticatedUser(user_id)
+
+
+@app.route('/')
+def root():
+    return render_template('root.html')
+
+
+@socketio.on('event')
+def handle_event(data):
+    print(data)
+
+
+if __name__ == '__main__':
+    socketio.run(app, allow_unsafe_werkzeug=True)
